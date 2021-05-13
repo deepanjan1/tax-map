2d4e5c623a22ab8b914f05f3615a845d8d226250
Deepanjan De <deep@deepcreations.net>
2021-05-12 20:24:11 -0400

    created a chart for tax rates

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,8 +1,11 @@
 import React, { useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
-import { getIncomeTaxesForOneState, getIncomeTaxesForAllStates } from '../utils/apis';
-import { buildColors } from '../utils/utilities';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+
+import { getIncomeTaxesForAllStates } from '../utils/apis';
+import { buildColors, formatter } from '../utils/utilities';
 import USAMap from "react-usa-map";
 
 
@@ -10,13 +13,27 @@ const useStyles = makeStyles((theme) => ({
     root: {
       flexGrow: 1,
     },
+    paper: {
+        marginTop: theme.spacing(1),
+        marginBottom: theme.spacing(1),
+        padding: theme.spacing(1),
+        flexGrow: 1,
+    },
   }));
 
 export default function Map({ filing, grossIncome }) {
     const classes = useStyles();
 
     const [response, setResponse] = React.useState(null);
-    const [chartConfig, setChartConfig] = React.useState(null);
+    const [snackBarInfo, setSnackbarInfo] = React.useState({
+        key: null,
+        fica: formatter.format(Math.round(null)),
+        federal: formatter.format(Math.round(null)),
+        state: formatter.format(Math.round(null)),
+        blendedRate: 0,
+    });
+    const [open, setOpen] = React.useState(false);
+
 
     useEffect(() => {
         const getAllTaxes = async() => await getIncomeTaxesForAllStates(filing, grossIncome)
@@ -29,13 +46,12 @@ export default function Map({ filing, grossIncome }) {
         getAllTaxes();
     }, [filing, grossIncome])
 
-    // useEffect(() => {
-    //     statesCustomConfig();
-    // }, [response])
+    const { key, fica, federal, state, blendedRate } = snackBarInfo;
 
     const mapHandler = (event) => {
         console.log(event.target.dataset.name);
       };
+    
       
     const statesCustomConfig = () => {
         var customConfig = {};
@@ -45,9 +61,19 @@ export default function Map({ filing, grossIncome }) {
             Object.keys(response).forEach(key => {
                 customConfig[key] = {
                     fill: response[key]['color']['amount'],
-                    clickHandler: (event) => console.log(`${key} clicked. Blended Tax Rate is ${
-                        Math.round(response[key]['blendedRate']['amount']*10000) / 100
-                    }%.`)
+                    clickHandler: (event) => {
+                        setSnackbarInfo({
+                            key: key,
+                            fica: response[key]['fica']['amount'],
+                            federal: response[key]['federal']['amount'],
+                            state: response[key]['state']['amount'],
+                            blendedRate: Math.round(response[key]['blendedRate']['amount']*10000 / 100),
+                        })
+                        setOpen(true);
+                    }
+                    // clickHandler: (event) => console.log(`${key} clicked. Blended Tax Rate is ${
+                    //     Math.round(response[key]['blendedRate']['amount']*10000) / 100
+                    // }%.`)
                 }
             })
         }
@@ -58,10 +84,22 @@ export default function Map({ filing, grossIncome }) {
         <div className={classes.root}>
             <Grid container direction="column" align="center">
                 <Grid item>
+                    <Paper 
+                        elevation={0}
+                        className={classes.paper}
+                        variant="outlined"
+                        >
+                        <Typography variant='h6'>{key} Rates</Typography>
+                        <Typography>FICA: ${fica}</Typography>
+                        <Typography>Federal Tax: ${federal}</Typography>
+                        <Typography>State Tax: ${state}</Typography>
+                        <Typography>Blended Tax Rate: {blendedRate}%</Typography>
+                    </Paper>
+                </Grid>
+                <Grid item>
                     <USAMap 
                         customize={statesCustomConfig()} 
                         onClick={mapHandler} 
-                        // title={'US Tax Map'}
                         />
                 </Grid>
                 {/* <Grid item>
